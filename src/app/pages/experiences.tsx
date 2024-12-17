6b502fcf168b59321e27ef7b28136dc7c2ac2be1
Burak849 <burakkurtulus018@gmail.com>
2024-12-17 07:45:37 +0300

    versiyon kayit .2

--- src/app/pages/experiences.tsx
+++ src/app/pages/experiences.tsx
@@ -4,13 +4,24 @@ import { relative } from 'path';
 
 const ExperienceSection = () => {
     return (
+
+        <div>
+            <h1 style={{ position: 'relative', textAlign: 'center', fontSize: 34, display: 'flex', justifyContent: 'center', color: 'white' }}>About me</h1>
         <div className={styles['spesifik']}>
 
-            <div>
-                <p style={{ position: 'relative', textAlign: 'center', fontSize: 34, display: 'flex', justifyContent: 'center', color: 'white' }}>About me</p>
-                <img src="../media/rmvd-bg.png" alt="About Me Image" className={styles['fotom']} />
 
-            </div>
+                <img src="../media/about-bg.gif" alt="About BG" className={styles['gif']} />
+                <p style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '2vh' }} >
+                    Lorem ipsum dolor sit amet. Sit provident nemo qui dicta assumenda est optio ipsa et rerum repudiandae ab asperiores repudiandae. Id fuga aliquid in ipsam eveniet et exercitationem perferendis est saepe dolorem ab laudantium sint et minus dolor. Et sunt saepe qui consectetur sint ut cupiditate dolorem. Hic repellendus neque ut sint libero ut explicabo nihil eos consequatur nesciunt.
+
+                    Eos incidunt natus et corporis atque qui dolore sint ex velit illo aut delectus iure et galisum impedit. Est dolores mollitia est voluptatem perferendis ad nemo libero qui possimus omnis? Et voluptatem aspernatur et dignissimos maxime sed dolores recusandae et voluptates nisi At quidem galisum? Est eius labore At voluptatum aliquid est quia quia At ducimus deleniti.
+
+                    Aut aliquid ipsa aut porro doloremque et delectus debitis et repellendus dolore ut animi facere in aperiam quis est aliquid sequi. Est laboriosam rerum eum dolores consequatur qui fugiat tenetur. Quo molestiae perspiciatis et nobis repellat sed sapiente recusandae et quis galisum est debitis delectus non dignissimos sapiente in quos quisquam. Sed odit ullam aut omnis possimus sed quia quisquam id reprehenderit quasi eos reprehenderit magni qui optio deleniti ex nulla quaerat?
+
+                </p>
+
+            
+        </div>
         </div>
 
         
--- src/app/pages/projects.tsx
+++ src/app/pages/projects.tsx
@@ -1,22 +1,82 @@
 import React from 'react';
 import styles from '../styles/Project.module.css'; 
+import styled from 'styled-components';
 
 const ProjectSection = () => {
-    return (
-        <div className={styles.spesifikk}>
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-        </div>
 
-        
+    return (
+        <StyledWrapper>
+            <div className="card">
+                <div className="img-container">
+                    <div className="img">
+                    </div>
+                    <div className="description card">
+                        <span className="title">
+                            Doctor's Referral Form
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </StyledWrapper>
     );
-};
+}
+
+const StyledWrapper = styled.div`
+  /* From uiverse.io by @FrM-bot */
+  .card {
+    width: 200px;
+    height: min-content;
+    transition: all .3s;
+    position: relative;
+    border-radius: .5rem 2rem;
+    box-shadow: 0px 15px 20px -5px rgba(0, 0, 0, 0.5);
+  }
+
+  .card:hover {
+    transform: scale(1.03);
+  }
+
+  .img {
+    transition: all .3s;
+    background: #ff0084;
+    background: -webkit-linear-gradient(to right, #33001b, #ff0084);
+    background: linear-gradient(to right, #33001b, #ff0084);
+    width: 100%;
+  }
+
+  .img:hover {
+    transform: scale(1.3);
+  }
+
+  .img-container {
+    display: grid;
+    border-radius: .5rem 2rem;
+    height: 270px;
+    overflow: hidden;
+  }
+
+  .description {
+    position: absolute;
+    bottom: .5rem;
+    left: .5rem;
+    text-align: start;
+    padding: .5rem 1em;
+    width: 90%;
+    transition: all 0.5s ease;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    backdrop-filter: blur(.1rem);
+    background-color: rgba(0, 0, 0, 0.2);
+    border-radius: .5rem 2rem;
+  }
+
+  .description:hover {
+    transform: perspective(100px) translateX(7px) rotateX(3deg) rotateY(3deg) scale(1);
+    box-shadow: none;
+  }
+
+  .title {
+    color: aliceblue;
+  }`;
 
 export default ProjectSection;
